ef306c820716668e8158dadf1526203645429d54
Rinat <bahmanrinat@yandex.ru>
2019-02-09 20:03:35 +0500

    babel

---
+++ dist/app.js
@@ -0,0 +1,67 @@
+/* вызов события при выделении текста */
+const speakText = function speakText(event) {
+  // поиск всех кнопок с классом voice на странице
+  const isButton = document.getElementsByClassName('voice');
+
+  /* получаем выделенный текст  */
+  const selObj = window.getSelection();
+  const utterance = new SpeechSynthesisUtterance(selObj);
+  utterance.lang = 'en-EN';
+
+  /* создание кнопки на странице  */
+  const addButton = document.createElement('button');
+  const icon = document.createElement('i');
+  const tag = event.target.tagName;
+  /* кнопка для прекращения озвучивания текста */
+  const stopSpeakButton = document.querySelector('.stopSpeak');
+  stopSpeakButton.addEventListener('click', () => {
+    speechSynthesis.pause();
+    stopSpeakButton.style.display = 'none';
+    addButton.style.display = 'block';
+  });
+
+  /* если выделен текст и нету  кнопки */
+  if (selObj.toString().length > 2 && !(tag === 'BUTTON' || tag === 'svg' || tag === 'path') && isButton.length < 1) {
+    /* кнопка для чтения */
+    addButton.className = 'voice';
+    icon.className = 'fas fa-volume-up';
+
+    /* получаем позицию, где произошел клик */
+    addButton.style.top = `${event.pageY + 10}px`;
+    addButton.style.left = `${event.pageX - 10}px`;
+
+    /* добавление кнопки в DOM-дерево */
+    document.body.appendChild(addButton);
+    addButton.appendChild(icon);
+    console.log('Кнопка создана!');
+
+    /* событие на кнопке */
+    addButton.addEventListener('click', () => {
+      /* озвучиваем выделенный текст */
+      speechSynthesis.resume();
+      speechSynthesis.speak(utterance);
+      /* скрываем кнопку воспроизведения */
+      addButton.style.display = 'none';
+
+      /* Показываем кнопку прекращения озвучки */
+      stopSpeakButton.style.display = 'block';
+      stopSpeakButton.style.top = `${event.pageY + 10}px`;
+      stopSpeakButton.style.left = `${event.pageX - 10}px`;
+    });
+    // eslint-disable-next-line brace-style
+  }
+
+  /* Удаление кнопки  */
+  else if (event.target.tagName === 'HTML') {
+      document.body.removeChild(isButton[0]);
+      stopSpeakButton.style.display = 'none';
+      speechSynthesis.pause();
+      console.log('Кнопка удалена!');
+    }
+};
+
+if (navigator.userAgent.search(/MSIE/) > 0 || navigator.userAgent.search(/NET CLR /) > 0) {
+  console.log('Internet Explorer не поддерживает чтение текста!');
+} else {
+  document.addEventListener('click', speakText);
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const cssScss = require('gulp-css-scss');
-const babeljs = require('@babel/register');
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      babel = require('gulp-babel'),
+      browserSync = require('browser-sync');
+
+/* преобразовываем js файлы в старый формат  */
+gulp.task("babel-run", function () {
+  return gulp.src("src/app.js")
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest("dist"));
+});
+
+/* автоматическое обновление страниц */
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+  browserSync({ // Выполняем browser Sync
+      server: { // Определяем параметры сервера
+          baseDir: './' // Директория для сервера - app
+      },
+      notify: false // Отключаем уведомления
+  });
+});
 
 /* конвертировать main.scss в main.css */
-gulp.task('sass', function () {
+gulp.task('scss', function() {
   return gulp.src('./style/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./style/css/'));
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./style/css/'))
+        .pipe(browserSync.reload({stream: true}));
  });
  
+ /* наблюдаем за изменениями в файлах */
+ gulp.task('watch',  gulp.series('browser-sync', 'scss'), function() {
+  gulp.watch('./style/scss/**/*.scss', ['scss']); // Наблюдение за scss файлами
+});
